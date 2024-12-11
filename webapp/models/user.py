c89b632b8b19c55f81ab1f7c1674a5960968be6d
wand3 <godswillbaiyewu@gmail.com>
2024-12-11 17:42:24 +0100

    user model

--- webapp/models/user.py
+++ webapp/models/user.py
@@ -1,7 +1,6 @@
 from typing import List, Optional
 from pydantic import BaseModel, Field
 from bson import ObjectId
-from werkzeug.security import generate_password_hash, check_password_hash
 from datetime import datetime, timedelta, timezone
 import secrets
 from webapp.database.db_engine import db
@@ -15,32 +14,6 @@ class User(BaseModel):
     token: str
     token_expiration: str
 
-    def set_password(self, password):
-        self.hashed_password = generate_password_hash(password)
-
-    def check_password(self, password):
-        return check_password_hash(self.hashed_password, password)
-
-    # def get_token(self, expires_in=9600):
-    #     now = datetime.now(timezone.utc)
-    #     if self.token and self.token_expiration.replace(
-    #             tzinfo=timezone.utc) > now + timedelta(seconds=60):
-    #         return self.token
-    #     self.token = secrets.token_hex(16)
-    #     self.token_expiration = now + timedelta(seconds=expires_in)
-    #     db.session.add(self)
-    #     return self.token
-    #
-    # def revoke_token(self):
-    #     self.token_expiration = datetime.now(timezone.utc) - timedelta(seconds=1)
-
-    # @staticmethod
-    # def check_token(token):
-    #     user = db.session.scalar(select(User).where(User.token == token))
-    #     if user is None or user.token_expiration.replace(
-    #             tzinfo=timezone.utc) < datetime.now(timezone.utc):
-    #         return None
-    #     return user
 
     def __repr__(self):
         return f"<User(full_name={self.full_name}, email={self.email}, id={self.id} )>"
