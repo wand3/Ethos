f04fde361d66e98749bccbe0009c0b519ec9c126
wand3 <godswillbaiyewu@gmail.com>
2024-12-11 06:40:53 +0100

    mongo init successful

---
+++ webapp/database/db_engine.py
@@ -0,0 +1,33 @@
+import os
+from fastapi import FastAPI
+from motor.motor_asyncio import AsyncIOMotorClient
+from contextlib import asynccontextmanager
+import logging
+
+
+# MongoDB's connection string (localhost, no authentication)
+MONGO_CONNECTION_STRING = "mongodb://127.0.0.1:27017/"
+DATABASE_NAME = "ethos"
+
+# Load the MongoDB connection string from the environment variable MONGODB_URI
+
+# Create a MongoDB client
+client = AsyncIOMotorClient(MONGO_CONNECTION_STRING)
+
+
+@asynccontextmanager
+async def db_lifespan(app: FastAPI):
+    # Startup
+    app.mongodb_client = AsyncIOMotorClient(MONGO_CONNECTION_STRING)
+    app.database = app.mongodb_client.get_default_database(DATABASE_NAME)
+    ping_response = await app.database.command("ping")
+    if int(ping_response["ok"]) != 1:
+        raise Exception("Problem connecting to database cluster.")
+    else:
+        logging.info("Connected to database cluster.")
+
+    yield
+
+    # Shutdown
+    app.mongodb_client.close()
+
--- webapp/main.py
+++ webapp/main.py
@@ -1,18 +1,18 @@
 from fastapi import FastAPI
 from webapp.routes import ethos_router
+from webapp.database.db_engine import db_lifespan
 
 
 def create_app() -> FastAPI:
-    app = FastAPI()
-
+    app: FastAPI = FastAPI(lifespan=db_lifespan)
     # Include routes
     app.include_router(ethos_router, tags=["Ethos"])
 
     # app.include_router(portfolio_router, prefix="/portfolio", tags=["Portfolio"])
 
-
     # Create tables
 
     return app
+
 
 app = create_app()
