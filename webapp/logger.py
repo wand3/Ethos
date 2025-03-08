56e4ede48ad09125afd25320de6b311b89ea81ba
Babawande <68830561+wand3@users.noreply.github.com>
2025-03-08 22:51:18 +0000

    remove filehandler for serverless hosting to prevent issues
    vercel

--- webapp/logger.py
+++ webapp/logger.py
@@ -9,13 +9,19 @@ formatter = logging.Formatter(fmt="%(asctime)s - %(levelname)s - %(message)s")
 
 # create handlers
 stream_handler = logging.StreamHandler(sys.stdout)
-file_handler = logging.FileHandler('app.log')
 
-stream_handler.setFormatter(formatter)
+"""
+remove filehandler for serverless hosting to prevent issues This error occurs because Vercel's serverless environment has a read-only filesystem,
+and your code is trying to write to a file (app.log) using logging.FileHandler
+file_handler = logging.FileHandler('app.log')
 file_handler.setFormatter(formatter)
+logger.handlers = [stream_handler, file_handler]
+"""
+stream_handler.setFormatter(formatter)
 
 # add handlers to the logger
-logger.handlers = [stream_handler, file_handler]
+logger.handlers = [stream_handler]
+
 
 # set log level
 logger.setLevel(logging.INFO)
